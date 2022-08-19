d0ca5c38ca780e21e094dbcdf9cd98dfa8f7cc9d
tech-shamp <shampytechnical@gmail.com>
2022-08-20 03:28:06 +0500

    restart everything

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,14 @@
 // Import Statements
-const express = require("express")
-const path = require("path")
-const ejs = require("ejs")
-require("./db/conn")
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
 
 // Required Variables
-const MainPath = path.join(__dirname, "../public")
-const app = express()
-const port = process.env.PORT || 999
-const ViewsPath = path.join(__dirname, "../templates/views")
-const PartialsPath = path.join(__dirname, "../templates/partials")
+const MainPath = path.join(__dirname, "../public");
+const app = express();
+const port = process.env.PORT || 999;
+const ViewsPath = path.join(__dirname, "../templates/views");
+const PartialsPath = path.join(__dirname, "../templates/partials");
 
 // Webiste Content
 const title = {
@@ -19,27 +18,27 @@ const title = {
   404: "Page Not Found",
 
   tag: " - Backend Website",
-}
+};
 
 // App Setup
-app.use(express.static(MainPath))
-app.set("view engine", "ejs")
-app.set("views", ViewsPath)
-ejs.resolveInclude("partials", PartialsPath)
+app.use(express.static(MainPath));
+app.set("view engine", "ejs");
+app.set("views", ViewsPath);
+ejs.resolveInclude("partials", PartialsPath);
 
 // Home URL
 app.get("/", (req, res) => {
   res.render("index", {
-    title: title.login + title.tag,
-  })
-})
+    title: title.home + title.tag,
+  });
+});
 
 // Not Found URL
 app.get("*", (req, res) => {
   res.render("404", {
     title: title[404],
-  })
-})
+  });
+});
 
 // Start Server
-app.listen(port, console.info(`Live on ${port}`))
+app.listen(port, console.info(`Live on ${port}`));
